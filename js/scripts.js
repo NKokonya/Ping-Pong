8549d464084daa8607e3dc1ce2104ab330aadd34
NKokonya <zinzi.k@gmail.com>
2017-11-16 17:24:54 +0300

    Added Animated GIF to header container

--- js/scripts.js
+++ js/scripts.js
@@ -7,25 +7,25 @@ function pingPong() {
 console.log($('#number-choice').val());
 var input = $('#number-choice').val();
 
-var fizzArray = [];
+var pingpongArray = [];
 for (i=1; i<=input; i++) {
 
 if (i % 15 === 0){
-  fizzArray.push("fizzbuzz");
+  pingpongArray.push("Ping Pong!");
 }
 else if (i % 5 === 0) {
-  fizzArray.push("buzz");
+  pingpongArray.push("Pong");
 }
 else if (i % 3 === 0){
-  fizzArray.push("fizz");
+  pingpongArray.push("Ping");
 }
-else fizzArray.push(i);
+else pingpongArray.push(i);
 }
-console.log(fizzArray);
+console.log(pingpongArray);
 
-fizzArray.forEach(function(thing) {
-  var newSpan = $( '<div class="fizz-buzz-item"><span>' + thing + '</span></div>');
-  if (thing === "fizz" || thing === "buzz" || thing === "fizzbuzz") {
+pingpongArray.forEach(function(thing) {
+  var newSpan = $( '<div class="ping-pong-output"><span>' + thing + '</span></div>');
+  if (thing === "ping" || thing === "pong" || thing === "pingpong") {
     newSpan.addClass(thing);}
 
 $('.js-results').append(newSpan);
